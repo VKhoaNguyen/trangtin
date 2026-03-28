28ca3c598e48ecad1d06b4b41ce0fe207d0d5851
BaoBao-chan <135817894+BaoBao-chan@users.noreply.github.com>
2026-03-28 11:55:30 +0700

    Cap nhat link Render cho Google API

--- routers/auth.js
+++ routers/auth.js
@@ -13,7 +13,7 @@ require('dotenv').config(); // Kích hoạt đọc file .env
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/callback"
+    callbackURL: "https://vietnewshub.onrender.com/auth/google/callback"
   },
   async function(accessToken, refreshToken, profile, cb) {
       try {
